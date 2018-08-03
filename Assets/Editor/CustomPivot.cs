9dd9323bc945238002ca93551a9a766c0751e615
nguyen huu thanh <htnguyen132@gmail.com>
2018-08-04 00:50:44 +0700

    update
    update project

---
+++ Assets/Editor/CustomPivot.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(CustomPivotMesh))]
+public class CustomPivot : Editor
+{
+
+    Mesh mesh;
+    MeshFilter meshfilter;
+    GameObject objCurrent, objLast;
+    Vector3 p, last_p;
+    Vector3[] vertices;
+    Vector3 poObjCurrent;
+    private static readonly string[] _dontIncludeMe = new string[] { "m_Script","m_name" };
+
+    ///<summary>
+    ///Hide script in Inspector
+    ///</summary>
+    /// <example> 
+    /// Method will active remove script current in Inspectory
+    /// <code>
+    ///  ...
+    ///   DrawPropertiesExcluding(serializedObject, _dontIncludeMe);
+    ///  ...
+    /// </code>
+    /// </example>
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        DrawPropertiesExcluding(serializedObject, _dontIncludeMe);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+    /// <summary>  
+    ///  This function update handle pivot of GameObject for Meshfilter
+    /// </summary> 
+	///<param name="b">Bound of mesh</param>  
+    void UpdatePivotVetor()
+    {
+        var b = mesh.bounds;
+        Vector3 offset = -1 * b.center;
+        p = last_p = new Vector3(offset.x / b.extents.x, offset.y / b.extents.y, offset.z / b.extents.z);
+    }
+
+    ///<summary>
+    ///Function callback when pointer mouse select GameObject in hierachy
+    ///</summary>
+    void OnSelectionChange()
+    {
+        if (Selection.activeTransform)
+        {
+
+            objCurrent = Selection.activeTransform.gameObject;
+            poObjCurrent = objCurrent.transform.position;
+            meshfilter = Selection.activeTransform.gameObject.GetComponent<MeshFilter>();
+            if (meshfilter)
+            {
+                mesh = meshfilter.mesh;
+                if (mesh)
+                    UpdatePivotVetor();
+            }
+            objCurrent.GetComponent<CustomPivotMesh>().hideFlags = HideFlags.HideAndDontSave;
+
+        }
+        else
+        {
+            objCurrent = null;
+        }
+        Repaint();
+    }
+    void OnSceneGUI()
+    {
+        if (Selection.activeTransform)
+        {
+            OnSelectionChange();
+        }
+
+        Handles.BeginGUI();
+        EditorGUILayout.BeginVertical("Box", GUILayout.Width(300));
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = Color.gray;
+        EditorGUILayout.Space();
+        style.fontSize = 15;
+        style.alignment = TextAnchor.MiddleCenter;
+        GUILayout.Label("Handle Pivot", style);
+        p.x = EditorGUILayout.Slider("X", p.x, -1, 1);
+        p.y = EditorGUILayout.Slider("Y", p.y, -1, 1);
+        p.z = EditorGUILayout.Slider("Z", p.z, -1, 1);
+
+
+        if (objCurrent)
+        {
+            if (mesh)
+            {
+                if (p != last_p)
+                {
+                    ConvertMesh();
+                }
+                if (GUILayout.Button("Center"))
+                {
+                    p = Vector3.zero;
+                    ConvertMesh();
+                }
+            }
+        }
+
+        EditorGUILayout.Space();
+        objCurrent = (GameObject)EditorGUILayout.ObjectField(objCurrent, typeof(GameObject));
+        if (objCurrent)
+        {
+            if (mesh)
+            {
+                GUILayout.Label("pivot: " + mesh.bounds.center);
+                GUILayout.Label("vertices: " + mesh.vertices.Length);
+                GUILayout.Label("triangles: " + mesh.triangles.Length);
+            }
+
+
+        }
+
+        EditorGUILayout.EndVertical();
+        Handles.EndGUI();
+        if (objCurrent)
+        {
+            Handles.color = Color.green;
+            Handles.DrawSphere(1, poObjCurrent, Quaternion.identity, 0.1f);
+            Handles.color = Color.red;
+            Handles.Label(poObjCurrent, "Pivot");
+        }
+
+    }
+
+
+
+    void ConvertMesh()
+    {
+        vertices = null;
+        Vector3 diff = Vector3.Scale(mesh.bounds.extents, last_p - p);
+        objCurrent.transform.position -= Vector3.Scale(diff, objCurrent.transform.localScale); //Move object position
+        vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] += diff;
+        }
+        mesh.vertices = vertices;
+
+        mesh.RecalculateBounds();
+        last_p = p;
+    }
+}
---
+++ Assets/Editor/CustomPivotSprite.cs
@@ -0,0 +1,273 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PivotSprite))]
+public class CustomPivotSprite : Editor
+{
+    enum typeLL { a, b };
+    typeLL typel = typeLL.a;
+    Vector3 poRoot, poLast;
+    Sprite sprite;
+    Bounds bound;
+    GameObject obj;
+    Vector2 pivotCutom = Vector2.zero;
+    Vector2 pivotCenter = new Vector2(0.5f, 0.5f);
+    Vector2 pointPivotCutom = Vector2.zero;
+    bool isPivotCutom = false;
+    SpriteRenderer spriterender;
+
+    // [MenuItem("Tools/Sprite")]
+    // public static void Init()
+    // {
+    //     GetWindow<CustomPivotSprite>("sprite pivot").Show();
+    // }
+    void OnEnable()
+    {
+        setObjSelectionTranfrom();
+
+    }
+    void setObjSelectionTranfrom()
+    {
+        if (Selection.activeTransform)
+        {
+            obj = Selection.activeTransform.gameObject;
+            obj.transform.localScale = new Vector3(1,1,1);
+            spriterender = obj.GetComponent<SpriteRenderer>();
+            sprite = spriterender.sprite;
+            bound = spriterender.bounds;
+            poRoot = obj.transform.position;
+         //   pointPivotCutom = poRoot;
+            pivotCutom = new Vector2(0.5f, 0.5f);
+            if (poLast != obj.transform.position)
+            {
+                poRoot = bound.center;
+
+            }
+            poLast = obj.transform.position;
+            pointPivotCutom = poLast;
+            float disX = (pointPivotCutom.x - poRoot.x);
+            float disY = (pointPivotCutom.y - poRoot.y);
+            pivotCutom.x = ((float)(disX + bound.extents.x) / (bound.extents.x * 2));
+            pivotCutom.y = ((float)(disY + bound.extents.y) / (bound.extents.y * 2));
+        }
+        else
+        {
+            Reset();
+        }
+
+        Repaint();
+    }
+    ///<Summary>
+    ///function call when selection is exsist object 
+    ///</Summary>
+    public void OnSelectionChange()
+    {
+        setObjSelectionTranfrom();
+
+    }
+    void OnFocus()
+    {
+        Debug.Log("windown is focus");
+        setObjSelectionTranfrom();
+        Debug.Log("updated poRoot complete");
+
+    }
+
+    void OnSceneGUI()
+    {
+
+        if (poLast != obj.transform.position)
+        {
+            setObjSelectionTranfrom();
+        }
+        Handles.BeginGUI();
+        EditorGUILayout.BeginVertical("Box", GUILayout.Width(300));
+        GUIStyle styple = new GUIStyle();
+        styple.fontSize = 20;
+        styple.alignment = TextAnchor.MiddleCenter;
+        styple.normal.textColor = Color.gray;
+        GUILayout.Label("Custom Pivot Sprite", styple);
+        GUILayout.Space(10);
+        GUILayout.Label("Select Pivot Sprite Renderer", "flow varPin tooltip");
+        GUILayout.Space(10);
+        GUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+
+        if (obj)
+        {
+            EditorGUILayout.BeginVertical();
+
+
+
+
+            if (GUILayout.Button("LeftTop"))
+            {
+                UpdatePivot(new Vector2(0, 1));
+                Repaint();
+
+            }
+            if (GUILayout.Button("LeftCenter"))
+            {
+                UpdatePivot(new Vector2(0, 0.5f));
+                Repaint();
+
+            }
+            if (GUILayout.Button("LeftBottom"))
+            {
+                UpdatePivot(Vector2.zero);
+                Repaint();
+
+            }
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.BeginVertical();
+
+
+            if (GUILayout.Button("MidTop"))
+            {
+                UpdatePivot(new Vector2(0.5f, 1f));
+                Repaint();
+
+            }
+            if (GUILayout.Button("MidCenter"))
+            {
+                UpdatePivot(new Vector2(0.5f, 0.5f));
+                Repaint();
+
+            }
+            if (GUILayout.Button("MidBottom"))
+            {
+                UpdatePivot(new Vector2(0.5f, 0f));
+                Repaint();
+
+            }
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.BeginVertical();
+
+
+            if (GUILayout.Button("RightTop"))
+            {
+                UpdatePivot(new Vector2(1, 1));
+                Repaint();
+
+            }
+            if (GUILayout.Button("RightCenter"))
+            {
+                UpdatePivot(new Vector2(1, 0.5f));
+                Repaint();
+
+            }
+            if (GUILayout.Button("RightBottom"))
+            {
+                UpdatePivot(new Vector2(1, 0));
+                Repaint();
+
+            }
+            EditorGUILayout.EndVertical();
+            if (GUILayout.Button("Cutom", GUILayout.ExpandHeight(true)))
+            {
+                isPivotCutom = true;
+                Repaint();
+
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10);
+            GUILayout.Label("Infomation Sprite Renderer", "flow varPin tooltip");
+            GUILayout.Space(10);
+
+            if (isPivotCutom)
+            {
+                EditorGUILayout.BeginVertical();
+                GUILayout.Label("---------------------------------------------------");
+                GUILayout.Label("pivotCutom: " + pivotCutom);
+                GUILayout.Label("pointPivotCutom: " + pointPivotCutom);
+                EditorGUILayout.BeginHorizontal();
+                if (GUILayout.Button("Apply", GUILayout.Width(70)))
+                {
+                    UpdatePivot(pivotCutom);
+                    isPivotCutom = false;
+                }
+                if (GUILayout.Button("Cancle", GUILayout.Width(70)))
+                {
+                    isPivotCutom = false;
+                }
+                EditorGUILayout.EndHorizontal();
+                GUILayout.Label("---------------------------------------------------");
+                EditorGUILayout.EndVertical();
+            }
+            else
+            {
+                if (sprite)
+                {
+                    pointPivotCutom = poLast;
+                  //  pivotCutom = new Vector2(0.5f, 0.5f);
+                }
+
+            }
+            if (spriterender)
+            {
+                GUILayout.Label("Name Sprite: " + sprite.texture.name);
+                GUILayout.Label("Center: " + bound.center);
+                GUILayout.Label("Extents: " + bound.extents);
+                GUILayout.Label("pivot: " + sprite.pivot);
+                GUILayout.Label("Rect: " + sprite.rect.size);
+                GUILayout.Label("PoRoot: " + poRoot);
+
+            }
+        }
+        else
+        {
+            GUILayout.Label("Object is null!!");
+
+        }
+        Handles.EndGUI();
+        if (isPivotCutom)
+        {
+            pointPivotCutom = Handles.PositionHandle(pointPivotCutom, Quaternion.identity);
+            pivotCutom = new Vector2((pointPivotCutom.x * 0.5f) / poRoot.x, (pointPivotCutom.y * 0.5f) / poRoot.y);
+            Handles.color = Color.red;
+            Handles.DrawSphere(1, pointPivotCutom, Quaternion.identity, 0.13f);
+            Handles.color = Color.green;
+            Handles.DrawSphere(1, pointPivotCutom, Quaternion.identity, 0.1f);
+            Handles.Label(pointPivotCutom,"Point Pivot");
+            float disX = (pointPivotCutom.x - poRoot.x);
+            float disY = (pointPivotCutom.y - poRoot.y);
+            pivotCutom.x = ((float)(disX + bound.extents.x) / (bound.extents.x * 2));
+            pivotCutom.y = ((float)(disY + bound.extents.y) / (bound.extents.y * 2));
+
+
+        }
+
+    }
+
+    void Reset()
+    {
+        spriterender = null;
+        obj = null;
+        sprite = null;
+    }
+
+    void UpdatePivot(Vector2 pivot)
+    {
+        if (!spriterender)
+        {
+            Debug.Log("Sprite Rendering is null");
+        }
+        else
+        {
+            Sprite sp = Sprite.Create(sprite.texture, sprite.rect, pivot, sprite.pixelsPerUnit);
+            string nameTexture = sprite.texture.name + " instance";
+            sp.name = nameTexture;
+            spriterender.sprite = sp;
+            bound = sp.bounds;
+            sprite = sp;
+            Vector3 center = -1 * bound.center;
+            Vector2 pivotCurrent = sp.pivot;
+            Vector3 poResult = (poRoot + new Vector3(center.x, center.y, 0));
+            obj.transform.position = poResult;
+        }
+    }
+
+
+
+}
